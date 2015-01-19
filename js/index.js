6fd092b395df64125cfd9bb0d6b22431eee84c22
Eric Mill <eric@konklone.com>
2015-01-18 21:57:33 -0500

    add a fun message to the console

--- js/index.js
+++ js/index.js
@@ -758,4 +758,29 @@
     parent.node().appendChild(child.node());
   }
 
+  // friendly console message
+
+  if (window._ie) {
+    console.log("Hi! Please poke around to your heart's content.");
+    console.log("");
+    console.log("If you find a bug or something, please report it at https://github.com/GSA/analytics.usa.gov/issues");
+    console.log("This is an open source, public domain project, and your contributions are very welcome.");
+  }
+
+  // otherwise, let's get fancy
+  else {
+    var styles = {
+      big: "font-size: 24pt; font-weight: bold;",
+      medium: "font-size: 10pt",
+      medium_bold: "font-size: 10pt; font-weight: bold",
+      medium_link: "font-size: 10pt; font-weight: bold; color: #18f",
+    };
+    console.log("%cHi! Please poke around to your heart's content.", styles.big);
+    console.log(" ");
+    console.log("%cIf you find a bug or something, please report it over at %chttps://github.com/GSA/analytics.usa.gov/issues", styles.medium, styles.medium_link);
+    console.log("%cThis is an open source, public domain project, and your contributions are very welcome.", styles.medium);
+
+  }
+
+
 })(this);
