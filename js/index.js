292584d435ed2daa6d2b1d29a13e1fe640865837
Eric Mill <eric.mill@gsa.gov>
2015-03-18 21:10:00 -0400

    mention analytics-reporter in the console

--- js/index.js
+++ js/index.js
@@ -795,10 +795,12 @@
 
   // friendly console message
 
+  // plain text for IE
   if (window._ie) {
     console.log("Hi! Please poke around to your heart's content.");
     console.log("");
     console.log("If you find a bug or something, please report it at https://github.com/GSA/analytics.usa.gov/issues");
+    console.log("Like it, but want a different front-end? The data reporting is its own tool: https://github.com/18f/analytics-reporter");
     console.log("This is an open source, public domain project, and your contributions are very welcome.");
   }
 
@@ -813,6 +815,7 @@
     console.log("%cHi! Please poke around to your heart's content.", styles.big);
     console.log(" ");
     console.log("%cIf you find a bug or something, please report it over at %chttps://github.com/GSA/analytics.usa.gov/issues", styles.medium, styles.medium_link);
+    console.log("%cLike it, but want a different front-end? The data reporting is its own tool: %chttps://github.com/18f/analytics-reporter", styles.medium, styles.medium_link);
     console.log("%cThis is an open source, public domain project, and your contributions are very welcome.", styles.medium);
 
   }
