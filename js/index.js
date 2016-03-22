245818b1f410659422068096d2606fbe8b4f2ddd
Eric Mill <eric.mill@gsa.gov>
2016-03-22 09:38:42 -0400

    fix some USPS case sensitive URLs

--- js/index.js
+++ js/index.js
@@ -13,6 +13,14 @@
     "irs.gov/forms-&-pubs": "https://www.irs.gov/Forms-&-Pubs",
     "ebenefits.va.gov/ebenefits/homepage": "https://www.ebenefits.va.gov/ebenefits/homepage",
 
+    // USPS is afflicted with a bad case of sensitivity :(
+    "m.usps.com/m/trackconfirmaction": "https://m.usps.com/m/TrackConfirmAction",
+    "tools.usps.com/go/trackconfirmaction_input": "https://tools.usps.com/go/TrackConfirmAction!input",
+    "m.usps.com/m/home": "https://m.usps.com/m/Home",
+    "reg.usps.com/entreg/loginaction_input?appurl=https://cns.usps.com/labelinformation.shtml": "https://reg.usps.com/entreg/LoginAction!input?appurl=https://cns.usps.com/labelinformation.shtml",
+    "tools.usps.com/go/ziplookupaction!input.action": "https://tools.usps.com/go/ZipLookupAction!input.action",
+    "cns.usps.com/labelinformation.shtml": "https://cns.usps.com/labelInformation.shtml",
+
     // for 7/30 days tabs
     "egov.uscis.gov": "https://egov.uscis.gov/casestatus/",
     "wrh.noaa.gov": "http://www.wrh.noaa.gov"
