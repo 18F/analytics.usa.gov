2deef1c1aa572a5ca1d21f9d6dfffccdac84fb59
Jonathan Hooper <jonathan.hooper@gsa.gov>
2019-09-25 09:27:44 -0400

    jeez, get it together

--- bin/deploy-ci.sh
+++ bin/deploy-ci.sh
@@ -5,8 +5,8 @@ set -e
 # Install CF
 curl https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.1/cf-cli_6.46.1_linux_x86-64.tgz | tar xzvf -
 sudo mv cf /usr/local/bin/cf
-# Log into cloud.gov
 
+# Log into cloud.gov
 cf api api.fr.cloud.gov
 cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
 
