5942fda0edeff2d0e6ef75e4239e1eb60a900265
Aaron D Borden <adborden@a14n.net>
2020-06-16 14:10:26 -0700

    Deploy to staging through CI
    When pushing commits to the staging branch, run the CI build as usual and then
    deploy to the analytics-staging application.

--- bin/deploy-ci.sh
+++ bin/deploy-ci.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+app=${1:-analytics}
+
 # Install CF
 curl https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.1/cf-cli_6.46.1_linux_x86-64.tgz | tar xzvf -
 sudo mv cf /usr/local/bin/cf
@@ -11,6 +13,6 @@ cf api api.fr.cloud.gov
 cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
 
 # Push the app
-cf v3-zdt-push analytics
+cf v3-zdt-push ${app}
 
 cf logout
