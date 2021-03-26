4dc2fe6182370044f757afeff4a8fc66f6eb77a9
Ryan Wold <64987852+ryanwoldatwork@users.noreply.github.com>
2021-03-26 13:30:16 -0700

    Develop (#807)
    * update jekyll and npm packages
    
    * deploy the develop branch to a staging environment
    
    * add kramdown gem
    
    due to failing build

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 
-gem 'jekyll', '~> 3.8.5'
-
+gem 'jekyll', '~> 3.9.0'
 gem 'bourbon', '~> 4.3.4'
+gem 'kramdown-parser-gfm'
 gem 'sass', '~> 3.4.24'
 gem 'neat', '~> 2.1.0'
--- bin/deploy-ci.sh
+++ bin/deploy-ci.sh
@@ -2,15 +2,24 @@
 
 set -e
 
-# Install CF
-curl https://s3-us-west-1.amazonaws.com/cf-cli-releases/releases/v6.46.1/cf-cli_6.46.1_linux_x86-64.tgz | tar xzvf -
-sudo mv cf /usr/local/bin/cf
+if [ "${CIRCLE_BRANCH}" == "master" ]; then
+  # Log into cloud.gov
+  cf api api.fr.cloud.gov
+  cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
 
-# Log into cloud.gov
-cf api api.fr.cloud.gov
-cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
+  # Push the app
+  cf v3-zdt-push analytics
 
-# Push the app
-cf v3-zdt-push analytics
+  cf logout
+fi
 
-cf logout
+if [ "${CIRCLE_BRANCH}" == "develop" ]; then
+  # Log into cloud.gov
+  cf api api.fr.cloud.gov
+  cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
+
+  # Push the app
+  cf v3-zdt-push analytics-staging
+
+  cf logout
+fi
