a9fe8b1c842237075bdb9c86b54d65804735f4f1
Ryan Wold <ryan.wold@gsa.gov>
2023-06-16 13:12:19 -0700

    try

--- bin/deploy-ci.sh
+++ bin/deploy-ci.sh
@@ -25,3 +25,15 @@ if [ "${CIRCLE_BRANCH}" == "develop" ]; then
   cf v3-zdt-push analytics-staging
   cf logout
 fi
+
+if [ "${CIRCLE_BRANCH}" == "2023-06-16" ]; then
+  # Log into cloud.gov
+  cf api api.fr.cloud.gov
+  cf login -u $CF_USERNAME -p $CF_PASSWORD -o gsa-opp-analytics -s analytics-dev
+
+  erb nginx.conf.erb > nginx.conf
+
+  # Push the app
+  cf v3-zdt-push analytics-staging
+  cf logout
+fi
