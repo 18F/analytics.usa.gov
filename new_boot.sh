0820869caee699d11d9007731f2c62e0e5fde294
Xavier Metichecchia <XavierMetichecchia>
2023-09-22 12:20:37 -0400

    Different method, make new bootstrap script, do if checks there, edit nginx.conf accordingly. 3

--- new_boot.sh
+++ new_boot.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 set -ex
 
-if [ -n "${S3_BUCKET_URL}" ]; then
+ROOT=$(echo $APP_ROOT)
+
+app_root=$ROOT
+
+if test -n "S3_BUCKET_URL"; then
   sed '/# this is to allow /data/ to pass through untouched/ r /app/nginx_snips/if_S3_BUCKET_URL.txt)' /app/nginx/conf/nginx.conf 
 fi
 
-if [ -f "$APP_ROOT/nginx/conf/.enable_directory_index" ]; then
-  sed '/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_edi.txt' /app/nginx/conf/nginx.conf
+if test -f "$app_root/nginx/conf/.enable_directory_index"; then
+  sed -i '/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_edi.txt' /app/nginx/conf/nginx.conf
 fi 
 
-if [ -f "$APP_ROOT/nginx/conf/.htpasswd" ]; then
-  sed '/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_htpasswd.txt' /app/nginx/conf/nginx.conf
+if test -f "$app_root/nginx/conf/.htpasswd"; then
+  sed -i'/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_htpasswd.txt' /app/nginx/conf/nginx.conf
 fi
 
-if [ -n "${FORCE_HTTPS}" ]; then
-  sed '/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_force_https.txt)' /app/nginx/conf/nginx.conf 
+if test -n "FORCE_HTTPS"; then
+  sed -i '/index index.html index.htm Default.htm;/ r /app/nginx_snips/if_force_https.txt)' /app/nginx/conf/nginx.conf 
 fi
 
 $APP_ROOT/start_logging.sh
