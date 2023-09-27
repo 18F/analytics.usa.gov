bfa398c6583c225eecd3db5e907226df647128fa
Xavier Metichecchia <XavierMetichecchia>
2023-09-27 10:47:52 -0400

    Try replacing nginx/conf.d/default.conf 2

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -2,4 +2,4 @@
 
 mv /home/vcap/app/testing.conf /home/vcap/app/nginx/conf.f/default.conf
 
-nginx -p $PWD -c /home/vcap/app/nginx/conf.d/default.conf"
+nginx -p $PWD -c /home/vcap/app/nginx/conf.d/default.conf
