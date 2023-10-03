62d6db79fe8d9bfc4c4eae40bc08516ba6e938ed
Nick Lyons <nick.lyons@gsa.gov>
2023-10-03 11:46:26 -0400

    First and second steps for integrating uswds before init uswds

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/* gulpfile.js */
+
+/**
+* Import uswds-compile
+*/
+const uswds = require("@uswds/compile");
+
+/**
+* USWDS version
+* Set the major version of USWDS you're using
+* (Current options are the numbers 2 or 3)
+*/
+uswds.settings.version = 3;
+
+/**
+* Path settings
+* Set as many as you need
+*/
+uswds.paths.dist.css = './assets/css';
+uswds.paths.dist.theme = './sass';
+
+/**
+* Exports
+* Add as many as you need
+*/
+exports.init = uswds.init;
+exports.compile = uswds.compile;
+exports.watch = uswds.watch;
