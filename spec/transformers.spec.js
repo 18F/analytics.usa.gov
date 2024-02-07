f100242423f330694de43b0776bce9174424db47
Michael Levin <michael.levin@gsa.gov>
2024-02-07 11:48:10 -0500

    Fix js specs that were marked pending

--- spec/transformers.spec.js
+++ spec/transformers.spec.js
@@ -2,14 +2,15 @@ import assert from 'assert';
 import transformers from '../js/lib/transformers';
 
 describe('transformers', () => {
-  const sampleArray = [{ key: 'bob', value: 1000 },
+  const sampleArray = [
+    { key: 'bob', value: 1000 },
     { key: 'harry', value: 300 },
     { key: 'tom', value: 2 },
   ];
 
   const proportionArray = [{ key: 'bob', value: 1000, proportion: 76.80491551459293 },
-    { key: 'harry', value: 300, proportion: 23.04147465437788 },
-    { key: 'tom', value: 2, proportion: 0.15360983102918588 },
+  { key: 'harry', value: 300, proportion: 23.04147465437788 },
+  { key: 'tom', value: 2, proportion: 0.15360983102918588 },
   ];
   describe('listify', () => {
     it('order a list by its key value pairs', () => {
@@ -24,24 +25,22 @@ describe('transformers', () => {
 
     it('should will include an undefined when not all list items have a value key', () => {
       const badArray = [{ key: 'bob', value: 1000 },
-        { key: 'harry', toons: 300 },
-        { key: 'tom', value: 2 },
+      { key: 'harry', toons: 300 },
+      { key: 'tom', value: 2 },
       ];
       assert.deepEqual(transformers.extractArrayValue(badArray), [1000, undefined, 2]);
     });
 
-    // eslint-disable-next-line no-undef
-    xit('will error if just an object', () => {
+    it('will error if just an object', () => {
       const badObject = { key: 'bob', value: 1000 };
-      assert.throws(transformers.extractArrayValue(badObject), Error);
+      assert.throws(() => { transformers.extractArrayValue(badObject) }, TypeError);
     });
   });
 
   describe('findProportionsOfMetric', () => {
-    // eslint-disable-next-line no-undef
-    xit('given a list of objects with some sort of numeric value, return a new list wth the orignal data and the % of the total numeric values', () => {
+    it('given a list of objects with some sort of numeric value, return a new list wth the orignal data and the % of the total numeric values', () => {
       function sampleArrayExtract(list) {
-        list.map(item => item.value);
+        return list.map(item => item.value);
       }
       assert.deepEqual(
         transformers.findProportionsOfMetric(sampleArray, sampleArrayExtract),
@@ -69,14 +68,14 @@ describe('transformers', () => {
 
     it('will group all values below a certain threshold', () => {
       const proportionArraySmall = [{ key: 'bob', value: 1000, proportion: 76.8 },
-        { key: 'harry', value: 300, proportion: 23.0 },
-        { key: 'tom', value: 2, proportion: 0.0015 },
-        { key: 'minnie', value: 2, proportion: 0.0025 },
+      { key: 'harry', value: 300, proportion: 23.0 },
+      { key: 'tom', value: 2, proportion: 0.0015 },
+      { key: 'minnie', value: 2, proportion: 0.0025 },
       ];
 
       const proportionArrayResult = [{ key: 'bob', value: 1000, proportion: 76.8 },
-        { key: 'harry', value: 300, proportion: 23.0 },
-        { key: 'Other', proportion: 0.004, children: [] },
+      { key: 'harry', value: 300, proportion: 23.0 },
+      { key: 'Other', proportion: 0.004, children: [] },
       ];
       assert.deepEqual(
         transformers.consolidateSmallValues(proportionArraySmall, 1),
@@ -86,14 +85,14 @@ describe('transformers', () => {
   });
 
   describe('toTopPercents', () => {
-    xit('returns the top percents given some data', () => {
-      const resultsArray = [{ key: 'bob', value: 1000, proportion: 76.80491551459293 },
-        { key: 'harry', value: 300, proportion: 23.04147465437788 },
-        { key: 'Other', proportion: 0.15360983102918588, children: [] },
+    it('returns the top percents given some data', () => {
+      const resultsArray = [{ key: 'bob', value: 1000, proportion: 76.92189350933063 },
+      { key: 'harry', value: 300, proportion: 23.07656805279919 },
+      { key: 'Other', proportion: 0.0015384378701866124, children: [] },
       ];
 
       assert.deepEqual(
-        transformers.toTopPercents({ totals: { os: { bob: 1000, tom: 2, harry: 300 } } }, 'os'),
+        transformers.toTopPercents({ totals: { os: { bob: 1000, tom: .02, harry: 300 } } }, 'os'),
         resultsArray,
       );
     });
