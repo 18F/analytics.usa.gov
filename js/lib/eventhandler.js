685bfdf2f293420b76fdb0cbabec86cc26aa67d6
Michael Levin <michael.levin@gsa.gov>
2024-10-04 16:05:53 -0400

    [Feature] Add second question to touchpoints page helpful survey

--- js/lib/eventhandler.js
+++ js/lib/eventhandler.js
@@ -31,37 +31,63 @@ export default function () {
   });
 
   document.addEventListener("onTouchpointsFormLoaded", (e) => {
-    // Adds event handlers for a specific form: 9ae710d3
+    // Adds event handlers for the "Was this page helpful?" form
     if (
       e.detail &&
       e.detail.formComponent
         .formComponent()
-        .getAttribute("data-touchpoints-form-id") === "9ae710d3"
+        .getAttribute("data-touchpoints-form-id") === "8fc3c209"
     ) {
       const yesButton = e.detail.formComponent
         .formComponent()
-        .querySelector('input.usa-button[value="yes"]');
+        .querySelector('input.usa-radio__input[value="Yes"]');
       const noButton = e.detail.formComponent
         .formComponent()
-        .querySelector('input.usa-button[value="no"]');
+        .querySelector('input.usa-radio__input[value="No"]');
+      const questions = e.detail.formComponent
+        .formComponent()
+        .querySelectorAll(".questions .question");
+      const header = e.detail.formComponent
+        .formComponent()
+        .querySelector("header");
+      const submitButton = e.detail.formComponent
+        .formComponent()
+        .querySelector('button.submit_form_button[type="submit"]');
 
-      yesButton.addEventListener("click", (event) => {
-        event.preventDefault();
+      if (questions.length > 0 && submitButton) {
+        questions[1].classList.add("hide");
+        submitButton.classList.add("hide");
+      }
+
+      const handleYesNoButtonClick = (clickEvent, response) => {
+        if (questions.length > 0 && header && submitButton) {
+          // Hide the first form question and the header. Show the second along
+          // with the submit button.
+          questions[0].classList.add("hide");
+          header.classList.add("hide");
+          questions[1].classList.remove("hide");
+          submitButton.classList.remove("hide");
+        }
+
+        // Send metrics for the yes/no button click
         gas4("was_this_helpful_submit", {
           event_category: "cx_feedback",
-          event_action: "was_this_page_helpful2",
-          event_label: "yes",
+          event_action: "was_this_page_helpful_v2.0",
+          event_label: response,
         });
-      });
+      };
 
-      noButton.addEventListener("click", (event) => {
-        event.preventDefault();
-        gas4("was_this_helpful_submit", {
-          event_category: "cx_feedback",
-          event_action: "was_this_page_helpful2",
-          event_label: "no",
+      if (yesButton) {
+        yesButton.addEventListener("click", (event) => {
+          handleYesNoButtonClick(event, "yes");
         });
-      });
+      }
+
+      if (noButton) {
+        noButton.addEventListener("click", (event) => {
+          handleYesNoButtonClick(event, "no");
+        });
+      }
     }
   });
 }
