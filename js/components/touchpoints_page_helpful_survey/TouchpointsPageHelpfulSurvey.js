184d9717d6443933e4b9843506b8ef84470d9301
Michael Levin <michael.levin@gsa.gov>
2024-10-11 16:47:10 -0400

    [Feature] Do not allow touchpoints form submission until 8 seconds have passed
    Attempt to avoid form submission by bots. Humans are unlikely to submit the form within 8 seconds
    of page load.

--- js/components/touchpoints_page_helpful_survey/TouchpointsPageHelpfulSurvey.js
+++ js/components/touchpoints_page_helpful_survey/TouchpointsPageHelpfulSurvey.js
@@ -1,5 +1,4 @@
 import { useEffect, React } from "react";
-//import PropTypes from "prop-types";
 
 /**
  * Creates the site TouchpointsPageHelpfulSurvey. Loads the JS code provided by
@@ -9,8 +8,9 @@ import { useEffect, React } from "react";
  * @returns {import('react').ReactElement} The rendered element
  */
 function TouchpointsPageHelpfulSurvey() {
+  const initialFormDelayMilliseconds = 8000;
+
   useEffect(() => {
-    // Hide the touchpoints footer when the form loads
     document.addEventListener("onTouchpointsFormLoaded", (e) => {
       if (
         e.detail &&
@@ -18,12 +18,78 @@ function TouchpointsPageHelpfulSurvey() {
           .formComponent()
           .getAttribute("data-touchpoints-form-id") === "8fc3c209"
       ) {
+        /**
+         * Set an initial timeout for the form. If the form is submitted too
+         * quickly (because it's a bot), then sendFeedback is a no-op
+         */
+        const originalSendFeedbackFunction =
+          e.detail.formComponent.sendFeedback;
+        /* eslint-disable no-empty */
+        e.detail.formComponent.sendFeedback = () => {};
+        /* eslint-enable no-empty */
+
+        setTimeout(() => {
+          e.detail.formComponent.sendFeedback = originalSendFeedbackFunction;
+        }, initialFormDelayMilliseconds);
+
         const touchpointsFooter = e.detail.formComponent
           .formComponent()
           .querySelector("footer.touchpoints-footer-banner");
+        const yesButton = e.detail.formComponent
+          .formComponent()
+          .querySelector('input.usa-radio__input[value="Yes"]');
+        const noButton = e.detail.formComponent
+          .formComponent()
+          .querySelector('input.usa-radio__input[value="No"]');
+        const questions = e.detail.formComponent
+          .formComponent()
+          .querySelectorAll(".questions .question");
+        const header = e.detail.formComponent
+          .formComponent()
+          .querySelector("header");
+        const submitButton = e.detail.formComponent
+          .formComponent()
+          .querySelector('button.submit_form_button[type="submit"]');
 
+        // Hide the touchpoints footer always
         if (touchpointsFooter) {
           touchpointsFooter.classList.add("hide");
+        }
+
+        // Hide the second question and submit button initially
+        if (questions.length > 0 && submitButton) {
+          questions[1].classList.add("hide");
+          submitButton.classList.add("hide");
+        }
+
+        const handleYesNoButtonClick = (clickEvent, response) => {
+          if (questions.length > 0 && header && submitButton) {
+            // Hide the first form question and the header. Show the second along
+            // with the submit button.
+            questions[0].classList.add("hide");
+            header.classList.add("hide");
+            questions[1].classList.remove("hide");
+            submitButton.classList.remove("hide");
+          }
+
+          // Send metrics for the yes/no button click
+          gas4("was_this_helpful_submit", {
+            event_category: "cx_feedback",
+            event_action: "was_this_page_helpful_v2.0",
+            event_label: response,
+          });
+        };
+
+        if (yesButton) {
+          yesButton.addEventListener("click", (event) => {
+            handleYesNoButtonClick(event, "yes");
+          });
+        }
+
+        if (noButton) {
+          noButton.addEventListener("click", (event) => {
+            handleYesNoButtonClick(event, "no");
+          });
         }
       }
     });
